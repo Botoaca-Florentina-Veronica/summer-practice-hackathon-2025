d6e247be779f73cacc1eb8ec289f419945d784a0
Vera <veraflorentina01@gmail.com>
2025-06-06 12:42:07 +0300

    adaugat continut in MainStage (+ buton de sign-up)

--- frontend/src/components/MainStage.jsx
+++ frontend/src/components/MainStage.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import peopleProgramming from '../assets/images/peopleProgramming.png';
 import './MainStage.css';
 
 const MainStage = () => {
+  const navigate = useNavigate();
   return (
     <div className="mainstage-bg">
       <div className="mainstage-container">
         <div className="mainstage-left">
           <h1>GitGud, locul unde să îți gestionezi toate proiectele!</h1>
-          <p className="mainstage-sub"></p>
-          <button className="mainstage-signup">Sign up</button>
+          <p className="mainstage-sub">Vorbești cu colegii, primesti feedback si dai sfaturi</p>
+          <button className="mainstage-signup" onClick={() => navigate('/login')}>Sign up</button>
         </div>
         <div className="mainstage-right">
           <img src={peopleProgramming} alt="People programming" className="mainstage-image" />
