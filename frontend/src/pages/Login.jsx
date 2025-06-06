90c88d807303596020f1b071a2f51006319637c2
Vera <veraflorentina01@gmail.com>
2025-06-06 11:23:50 +0300

    adaugat pagina de login pentru utilizatori

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import googleIcon from "../assets/images/google-icon.png";
-import facebookIcon from "../assets/images/facebook-icon.png";
-import appleIcon from "../assets/images/apple-icon.png";
+import googleIcon from "../assets/images/google.png";
+import facebookIcon from "../assets/images/facebook.png";
+import appleIcon from "../assets/images/apple.png";
 import "./Login.css";
 
 const Login = () => {
@@ -9,21 +9,27 @@ const Login = () => {
     <div className="login-container">
       <h2>Intră în cont</h2>
       <button className="google-btn">
-        <span className="google-icon"></span> Continuă cu Google
+        <img src={googleIcon} alt="Google" className="icon-img" /> Continuă cu
+        Google
       </button>
       <button className="facebook-btn">
-        <span className="facebook-icon"></span> Continuă cu Facebook
+        <img src={facebookIcon} alt="Facebook" className="icon-img" /> Continuă cu
+        Facebook
       </button>
       <button className="apple-btn">
-        <span className="apple-icon"></span> Continuă cu Apple
+        <img src={appleIcon} alt="Apple" className="icon-img" /> Continuă cu Apple
       </button>
       <div className="separator">SAU</div>
       <input type="email" placeholder="Adresa ta de email" />
       <input type="password" placeholder="Parola" />
       <button className="login-btn">Intră în cont</button>
       <div className="login-footer">
-        <a href="#" className="forgot">Ai uitat parola?</a>
-        <a href="#" className="create">Creează cont</a>
+        <a href="#" className="forgot">
+          Ai uitat parola?
+        </a>
+        <a href="#" className="create">
+          Creează cont
+        </a>
       </div>
     </div>
   );
